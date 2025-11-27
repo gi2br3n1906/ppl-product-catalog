8ad1c78128ae5460ec36d5432e8d2650b3ad6415
KyoneCode <hadyan.kp@gmail.com>
2025-11-27 21:03:43 +0700

    benerin db gambar, nambahin upload produk, layouting ulang dashboard seller, ngubek katalog dikit

---
+++ database/migrations/2025_11_27_121013_create_product_images_table.php
@@ -0,0 +1,33 @@
+    <?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+{
+    Schema::create('product_images', function (Blueprint $table) {
+        $table->id();
+        // Ini kunci utamanya: Relasi ke tabel products
+        // onDelete('cascade') artinya jika produk dihapus, gambarnya ikut terhapus otomatis
+        $table->foreignId('product_id')->constrained('products')->onDelete('cascade');
+
+        $table->string('image_path'); // Lokasi file gambar
+        $table->boolean('is_primary')->default(false); // Opsional: untuk nandain gambar utama
+        $table->timestamps();
+    });
+}
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_images');
+    }
+};
